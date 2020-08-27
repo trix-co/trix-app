f0bf33b0d6d6712769baa85f8613a4734f82b55c
David Feldman <davidelanfeldman@gmail.com>
2020-08-27 12:58:44 -0700

    saving before clean

--- src/home/profile/Profile.js
+++ src/home/profile/Profile.js
@@ -1,27 +1,27 @@
 // @flow
 import autobind from "autobind-decorator";
 import * as React from "react";
-import {View, StyleSheet, Dimensions, TouchableOpacity, Image} from "react-native";
-import {Feather as Icon} from "@expo/vector-icons";
-import {inject, observer} from "mobx-react/native";
+import { View, StyleSheet, Dimensions, TouchableOpacity, Image } from "react-native";
+import { Feather as Icon } from "@expo/vector-icons";
+import { inject, observer } from "mobx-react/native";
 import Constants from "expo-constants";
-import {LinearGradient} from "expo-linear-gradient";
-import {NavigationEvents} from "react-navigation";
+import { LinearGradient } from "expo-linear-gradient";
+import { NavigationEvents } from "react-navigation";
 
 import ProfileStore from "../ProfileStore";
 
-import {Text, Avatar, Theme, Images, Feed, FeedStore} from "../../components";
-import type {FeedEntry} from "../../components/Model";
-import type {ScreenProps} from "../../components/Types";
+import { Text, Avatar, Theme, Images, Feed, FeedStore } from "../../components";
+import type { FeedEntry } from "../../components/Model";
+import type { ScreenProps } from "../../components/Types";
 
 type InjectedProps = {
     profileStore: ProfileStore,
-    userFeedStore: FeedStore
+    userFeedStore: FeedStore,
 };
 
-@inject("profileStore", "userFeedStore") @observer
+@inject("profileStore", "userFeedStore")
+@observer
 export default class ProfileComp extends React.Component<ScreenProps<> & InjectedProps> {
-
     componentDidMount() {
         this.loadFeed();
     }
@@ -30,7 +30,7 @@ export default class ProfileComp extends React.Component<ScreenProps<> & Injecte
 
     @autobind
     settings() {
-        const {profile} = this.props.profileStore;
+        const { profile } = this.props.profileStore;
         this.props.navigation.navigate("Settings", { profile });
     }
 
@@ -46,18 +46,15 @@ export default class ProfileComp extends React.Component<ScreenProps<> & Injecte
     }
 
     render(): React.Node {
-        const {navigation, userFeedStore, profileStore} = this.props;
-        const {profile} = profileStore;
+        const { navigation, userFeedStore, profileStore } = this.props;
+        const { profile } = profileStore;
         return (
             <View style={styles.container}>
                 <NavigationEvents onWillFocus={this.loadFeed} />
-                <LinearGradient
-                    colors={["#5cc0f1", "#d6ebf4", "white"]}
-                    style={styles.gradient}
-                />
+                <LinearGradient colors={["#5cc0f1", "#d6ebf4", "white"]} style={styles.gradient} />
                 <Feed
                     bounce={false}
-                    ListHeaderComponent={(
+                    ListHeaderComponent={
                         <View style={styles.header}>
                             <Image style={styles.cover} source={Images.cover} />
                             <TouchableOpacity onPress={this.settings} style={styles.settings}>
@@ -66,14 +63,18 @@ export default class ProfileComp extends React.Component<ScreenProps<> & Injecte
                                 </View>
                             </TouchableOpacity>
                             <View style={styles.title}>
-                                <Text type="large" style={styles.outline}>{profile.outline}</Text>
-                                <Text type="header2" style={styles.name}>{profile.name}</Text>
+                                <Text type="large" style={styles.outline}>
+                                    {profile.outline}
+                                </Text>
+                                <Text type="header2" style={styles.name}>
+                                    {profile.name}
+                                </Text>
                             </View>
                             <Avatar size={avatarSize} style={styles.avatar} {...profile.picture} />
                         </View>
-                    )}
+                    }
                     store={userFeedStore}
-                    {...{navigation}}
+                    {...{ navigation }}
                 />
             </View>
         );
@@ -81,47 +82,47 @@ export default class ProfileComp extends React.Component<ScreenProps<> & Injecte
 }
 
 const avatarSize = 100;
-const {width} = Dimensions.get("window");
-const {statusBarHeight} = Constants;
+const { width } = Dimensions.get("window");
+const { statusBarHeight } = Constants;
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
     },
     gradient: {
         position: "absolute",
         top: 0,
         left: 0,
         right: 0,
-        height: width
+        height: width,
     },
     header: {
-        marginBottom: (avatarSize * 0.5) + Theme.spacing.small
+        marginBottom: avatarSize * 0.5 + Theme.spacing.small,
     },
     cover: {
         width,
-        height: width
+        height: width,
     },
     avatar: {
         position: "absolute",
         right: Theme.spacing.small,
-        bottom: -avatarSize * 0.5
+        bottom: -avatarSize * 0.5,
     },
     settings: {
         position: "absolute",
         top: statusBarHeight + Theme.spacing.small,
         right: Theme.spacing.base,
         backgroundColor: "transparent",
-        zIndex: 10000
+        zIndex: 10000,
     },
     title: {
         position: "absolute",
         left: Theme.spacing.small,
-        bottom: 50 + Theme.spacing.small
+        bottom: 50 + Theme.spacing.small,
     },
     outline: {
-        color: "rgba(255, 255, 255, 0.8)"
+        color: "rgba(255, 255, 255, 0.8)",
     },
     name: {
-        color: "white"
-    }
+        color: "white",
+    },
 });
